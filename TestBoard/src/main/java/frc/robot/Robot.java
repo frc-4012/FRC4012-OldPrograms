323862869706a87f4c591006087d529231c6485a
djpgamer7 <dpear0915@gmail.com>
2019-09-03 10:23:26 -0400

    Added the test board code

---
+++ TestBoard/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Robot extends TimedRobot {
+
+    public DifferentialDrive myRobot;
+    public Joystick stickL, stickR;
+
+    @Override
+    public void robotInit(){
+
+    }
+
+    @Override
+    public void autonomousInit(){
+
+    }
+
+    @Override
+    public void teleopPeriodic(){
+
+    }
+}
