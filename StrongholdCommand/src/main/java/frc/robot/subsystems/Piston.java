af5a165c31dbf50c14c9aa37855d8099bc604009
Daniel Pearson <dpear0915@gmail.com>
2019-09-26 09:59:10 -0400

    Update Piston.java

--- StrongholdCommand/src/main/java/frc/robot/subsystems/Piston.java
+++ StrongholdCommand/src/main/java/frc/robot/subsystems/Piston.java
@@ -10,14 +10,31 @@ import frc.robot.commands.TankDrive;
 /**
  * Add your docs here.
  */
-public class DriveTrain extends Subsystem {
+public class Piston extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   RobotMap map = new RobotMap();
+  sol1 = new DoubleSolenoid(0,1);
+  
+  public Piston(){
+    sol1.set(DoubleSolenoid.Value.kOff);
+  }
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+  
+  public void turnOff(){
+    sol1.set(DoubleSolenoid.Value.kOff);
+  }
+  
+  public void extend(){
+    sol1.set(DoubleSolenoid.Value.kForward);
+  }
+  
+  public void retract(){
+    sol2.set(DoubleSolenoid.Value.kReverse);
   }
 }
