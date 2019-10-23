704b98f2639f2719836c33585d0f4c29c181a892
Daniel Pearson <dpear0915@gmail.com>
2019-10-23 09:58:34 -0400

    Update Distance.java

--- functions/Distance.java
+++ functions/Distance.java
@@ -1,6 +1,10 @@
 package com.team4012.frc2020.functions;
 
-public class Distance {
+import edu.wpi.first.wpilibj.Ultrasonic;
+public class Ultra {
+  private Ultrasonic ultra = new Ultrasonic(1,2,3);
   
-  
+  public void getDist(){
+    return ultra.getDistance();
+  }
 }
