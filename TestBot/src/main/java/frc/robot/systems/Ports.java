7624715dd730faee22a8010224134f03ccd3a70c
Daniel Pearson <dpear0915@gmail.com>
2019-11-19 10:41:14 -0500

    Update Ports.java

--- TestBot/src/main/java/frc/robot/systems/Ports.java
+++ TestBot/src/main/java/frc/robot/systems/Ports.java
@@ -1,6 +1,10 @@
 package frc.robot.systems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.Robot;
+
 public class Ports {
-
-
+  private static WPI_TalonSRX rightDriveTrainFront;
+  
+  private static WPI_TalonSRX rightDriveTrainBack;
 }
