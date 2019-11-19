405eef577252ce75f49161a9931d4ecab4342919
Daniel Pearson <dpear0915@gmail.com>
2019-11-19 14:01:56 -0500

    Update and rename RobotMap.java to Constants.java

--- TestBot/src/main/java/frc/robot/Constants.java
+++ TestBot/src/main/java/frc/robot/Constants.java
@@ -13,7 +13,7 @@ package frc.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
+public class Constants  {
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
