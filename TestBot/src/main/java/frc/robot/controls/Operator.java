dc8a9c9a2dba3cf8e5609ba1ed3a7c462dbac965
Daniel Pearson <dpear0915@gmail.com>
2019-11-19 14:34:25 -0500

    Update Operator.java

--- TestBot/src/main/java/frc/robot/controls/Operator.java
+++ TestBot/src/main/java/frc/robot/controls/Operator.java
@@ -1,5 +1,8 @@
 package frc.robot.controls;
 
 public class Operator {
-
+  private Joystick stickL = new Joystick(Constants.LEFT_JOYSTICK);
+  private Joystick stickR = new Joystick(Constants.RIGHT_JOYSTICK);
+                                         
+     
 }
