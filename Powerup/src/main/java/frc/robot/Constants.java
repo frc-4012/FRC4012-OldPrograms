43c09d2a135da1227fab2f102dc98c68bf11654b
Daniel Pearson <dpear0915@gmail.com>
2019-11-20 10:58:32 -0500

    Update Constants.java

--- Powerup/src/main/java/frc/robot/Constants.java
+++ Powerup/src/main/java/frc/robot/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
     public static final int FRONT_RIGHT = 1;
     public static final int BACK_LEFT = 2;
     public static final int BACK_RIGHT = 3;
+    public static final int INTAKE_LEFT = 4;
+    public static final int INTAKE_RIGHT = 5;
     
     //Inputs
     public static final int LEFT_JOY = 0;
@@ -14,8 +16,14 @@ public class Constants {
     //Sensors
     public static final int INTAKE_ULTRA1 = 0;
     public static final int INTAKE_ULTRA2 = 1;
+    
+    //Buttons
+    public static final int INTAKE_BUTTON = 3;
+    public static final int OUTAKE_BUTTON = 5;
+    public static final int ELEVATOR_LIFT = 4;
+    public static final int ELEVATOR_RETRACT = 6;
 
-    //PID
+    //PIDS
     public static final double elevator_kP = 1;
     public static final double elevator_kI = 0.001;
     public static final double elevator_kD = 0;
